b7b08d1c7e1e6da6f35263bbb8ea6624b9a720db
Nicholas <nickc01@outlook.com>
2023-03-09 18:08:54 -0600

    Added Avalonia to the project
    Got the initial layout of the Main Window done. Still need to do the Login WIndow

---
+++ ScratchDL.GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Controls.Primitives;
+using System.Collections.Generic;
+
+namespace ScratchDL.GUI.ViewModels
+{
+    public record class ProjectEntry
+    (
+        bool Selected,
+        long ID,
+        string Name,
+        string Creator
+    );
+
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public List<ProjectEntry> ProjectEntries => new List<ProjectEntry>
+        {
+            new ProjectEntry(false,8062448201,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,8062448202,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(false,8062448203,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,8062448204,"Ropes","WingDingWarrior89"),new ProjectEntry(false,80624482011,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,8062448205,"Ropes","WingDingWarrior89"),new ProjectEntry(false,80624482012,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,8062448206,"Ropes","WingDingWarrior89"),new ProjectEntry(false,80624482013,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,8062448207,"Ropes","WingDingWarrior89"),new ProjectEntry(false,80624482014,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,8062448208,"Ropes","WingDingWarrior89"),new ProjectEntry(false,80624482015,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,8062448209,"Ropes","WingDingWarrior89"),new ProjectEntry(false,80624482016,"Ropes","WingDingWarrior89"),
+            new ProjectEntry(true,80624482010,"Ropes","WingDingWarrior89")
+        };
+
+        public void DisplayLoginWindow()
+        {
+
+        }
+    }
+}
---
+++ ScratchDL.GUI/Views/LoginWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ScratchDL.GUI.Views
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
